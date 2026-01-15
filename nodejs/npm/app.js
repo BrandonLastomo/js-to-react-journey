5834e7ff73178553381f9a98b67293f66f511d12
Brandon <brandonlastomo1@gmail.com>
2026-01-15 08:58:09 +0700

    using validator, chalk, and nodemon packages with npm in nodejs

--- nodejs/npm/app.js
+++ nodejs/npm/app.js
@@ -1,1 +1,9 @@
-console.log("NPM");
+const chalk = require("chalk");
+const validator = require("validator");
+// validator
+let dummy = "lorem ipsum dolor sit amet";
+console.log(validator.contains(dummy, "sit")); // check if dummy contains sit = true
+console.log(validator.isStrongPassword("admin123")); // check if admin123 is a strong password = false
+// chalk (ver 4.1.0)
+console.log(chalk.blue("hello"));
+console.log(chalk`hello {bgRed.bold World}!`); // template literal
