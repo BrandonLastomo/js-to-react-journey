2da276916675493fcee7ecde3ffc7d891649722e
Brandon <brandonlastomo1@gmail.com>
2026-01-20 17:04:29 +0700

    using ejs template engine for expressJS views

---
+++ nodejs/ejs-template-engine/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// use ejs
+app.set("view engine", "ejs");
+
+const datas = [
+  { name: "Baron", age: 18 },
+  { name: "Yoru", age: 28 },
+  { name: "Yor", age: 30 },
+];
+
+app.get("/", (req, res) => {
+  // display generic file
+  res.render("index", { title: "Home", datas });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
+
+app.get("/json", (req, res) => {
+  // display json
+  res.json({ name: "baron" });
+});
+
+app.get("/user/:id", (req, res) => {
+  // display param value
+  res.send(`Param value is ${req.params.id}`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
